966105e78e0118fe0ff95536d033a633f0b690c6
Bilal Syed <bsyed7633@conestogac.on.ca>
2025-10-17 17:02:35 -0400

    setup the colours and dimensions for drawing area and toolbar.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let toolBarWidth = 150;
+let borderWidth = 10;
+
 function setup() {
-  createCanvas(400, 400);
+  setupScreenLayout();
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function setupScreenLayout() {
+  createCanvas(windowWidth, windowHeight);
+  background(0,0,0);
+
+  fill('#FFFFFF');
+  rect(0, 0, toolBarWidth, windowHeight);
+  
+  fill('#686868ff');
+  rect(toolBarWidth, 0, borderWidth, windowHeight);
 }
